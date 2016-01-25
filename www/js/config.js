2bcd2e0731b96b5adeae12f80c524c86789fc74e
Ricardo Campos <ricardo@ewok.cl>
2016-01-25 16:59:08 -0300

    design fixes

--- www/js/config.js
+++ www/js/config.js
@@ -1,2 +1,9 @@
 var deezer_api_key = "e578f30dfee96be239aa95680c2f1b08";
 var url_http = "http://localhost:8100/";
+
+//Add scrolling capabilities (scroll in browser) within dev enviroment
+var css = 'html{overflow-y: auto !important;}',
+head = document.head || document.getElementsByTagName('head')[0],
+style = document.createElement('style');
+style.appendChild(document.createTextNode(css));
+head.appendChild(style);
